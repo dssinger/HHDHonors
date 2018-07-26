eea73a7453995333102abd3778098b410120cb77
David Singer <david@dsinger.com>
2018-07-26 15:57:24 -0700

    Put parms file processing in ONE place

---
+++ parms.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Read the honors.yaml file and set up appropriate parameters """
+import yaml
+class Parms:
+    def __init__(self):
+        self.contents = []
+        with open('honors.yaml', 'r') as f:
+            parms = yaml.load(f)
+        # Promote values to attributes
+        for item in parms:
+            self.contents.append(item)
+            setattr(self, item, parms[item])
+    
+if __name__ == '__main__':
+    parms = Parms()
+    for item in parms.contents:
+        print('%s = %s' % (item, getattr(parms,item)))
+    
+            
